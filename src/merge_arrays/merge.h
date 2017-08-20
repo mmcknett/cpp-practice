37c2aff32712d23217a1f4431f7d6aa80e326f9d
Matt McKnett <mmcknett@gmail.com>
2017-08-20 00:40:38 -0700

    Added merge function and some tests.  The most recent test currently fails.

---
+++ src/merge_arrays/merge.h
@@ -0,0 +1,11 @@
+#ifndef MERGE_H_INCLUDED
+#define MERGE_H_INCLUDED
+
+// Given two sorted arrays, merge them into a new sorted array.
+template <typename T>
+T* merge(T const lhs[], size_t lhsSize, T const rhs[], size_t rhsSize)
+{
+    return nullptr;
+}
+
+#endif /* end of include guard: MERGE_H_INCLUDED */
---
+++ src/merge_arrays/merge_arrays_test.cpp
@@ -0,0 +1,49 @@
+#define BOOST_TEST_MODULE merge_arrays Unit Tests
+#define BOOST_TEST_DYN_LINK
+#include <boost/test/unit_test.hpp>
+
+#include "merge.h"
+
+BOOST_AUTO_TEST_CASE(Merge_TwoEmptyFloatArrays_ReturnsNull)
+{
+    // Arrange
+    float array1[] = {};
+    float array2[] = {};
+    size_t size1 = 0;
+    size_t size2 = 0;
+
+    // Act
+    float* result = merge(array1, size1, array2, size2);
+
+    // Assert
+    BOOST_CHECK(!result);
+    delete[] result;
+}
+
+BOOST_AUTO_TEST_CASE(Merge_OneEmptyOneSortedArray_ReturnsCopyOfSortedArray)
+{
+    // Arrange
+    float array1[] = {};
+    float array2[] = {1.0f, 2.0f, 3.0f};
+    size_t size1 = 0;
+    size_t size2 = 3;
+
+    // Act
+    float* result = merge(array1, size1, array2, size2);
+
+    // Assert
+    BOOST_CHECK(std::equal(std::begin(array2), std::end(array2), result));
+    delete[] result;
+}
+
+// BOOST_AUTO_TEST_CASE(Merge_TwoFloatArraysLengthThree_MergesArrays)
+// {
+//     // Arrange
+//     float array1[] = {2.0f, 3.5f, 6.0f};
+//     float array2[] = {3.0f, 4.0f, 4.5f};
+//
+//     // Act
+//
+//     // Assert
+//     BOOST_TEST(false);
+// }
