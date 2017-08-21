a2b262f4469803cd7be81f4bc03bfc3b50f7e587
Matt McKnett <mmcknett@gmail.com>
2017-08-20 22:20:38 -0700

    Added the implementation for the base case where either the lhs or rhs array is empty.

--- src/merge_arrays/merge.h
+++ src/merge_arrays/merge.h
@@ -3,9 +3,29 @@
 
 // Given two sorted arrays, merge them into a new sorted array.
 template <typename T>
-T* merge(T const lhs[], size_t lhsSize, T const rhs[], size_t rhsSize)
+T* merge(const T lhs[], size_t lhsSize, const T rhs[], size_t rhsSize)
 {
-    return nullptr;
+    if (lhsSize + rhsSize == 0)
+    {
+        return nullptr;
+    }
+
+    T* result = new T[lhsSize + rhsSize];
+    T* currResult = result;
+    const T* lhsEnd = lhs + lhsSize;
+    const T* rhsEnd = rhs + rhsSize;
+
+    for (const T* currRhs = rhs; currRhs != rhsEnd; ++currRhs)
+    {
+        *(currResult++) = *currRhs;
+    }
+
+    for (const T* currLhs = lhs; currLhs != lhsEnd; ++currLhs)
+    {
+        *(currResult++) = *currLhs;
+    }
+
+    return result;
 }
 
 #endif /* end of include guard: MERGE_H_INCLUDED */
--- src/merge_arrays/merge_arrays_test.cpp
+++ src/merge_arrays/merge_arrays_test.cpp
@@ -24,8 +24,8 @@ BOOST_AUTO_TEST_CASE(Merge_OneEmptyOneSortedArray_ReturnsCopyOfSortedArray)
 {
     // Arrange
     float array1[] = {};
-    float array2[] = {1.0f, 2.0f, 3.0f};
     size_t size1 = 0;
+    float array2[] = {1.0f, 2.0f, 3.0f};
     size_t size2 = 3;
 
     // Act
@@ -33,6 +33,22 @@ BOOST_AUTO_TEST_CASE(Merge_OneEmptyOneSortedArray_ReturnsCopyOfSortedArray)
 
     // Assert
     BOOST_CHECK(std::equal(std::begin(array2), std::end(array2), result));
+    delete[] result;
+}
+
+BOOST_AUTO_TEST_CASE(Merge_OneSortedOneEmptyArray_ReturnsCopyOfSortedArray)
+{
+    // Arrange
+    float array1[] = {1.0f, 2.0f, 3.0f};
+    size_t size1 = 3;
+    float array2[] = {};
+    size_t size2 = 0;
+
+    // Act
+    float* result = merge(array1, size1, array2, size2);
+
+    // Assert
+    BOOST_CHECK(std::equal(std::begin(array1), std::end(array1), result));
     delete[] result;
 }
 
