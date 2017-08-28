834d1a614eb88fc5181cc9e8368942e0cefdd1ba
Matt McKnett <mmcknett@gmail.com>
2017-08-27 17:57:09 -0700

    Added cases for when one vector or the other is empty.

--- src/merge_arrays/merge_vectors.h
+++ src/merge_arrays/merge_vectors.h
@@ -2,12 +2,43 @@
 #define MERGE_VECTORS_H_INCLUDED
 
 #include <vector>
+#include <iostream>
+
+template <typename T>
+void printVector(const std::vector<T>& result)
+{
+    std::cerr << "Result: [";
+    for (auto it = std::begin(result); it != std::end(result); ++it)
+    {
+        std::cerr << *it << " ";
+    }
+    std::cerr << "]" << std::endl;
+}
 
 // Given two sorted vectors, merge them into a new sorted vector.
 template <typename T>
 std::vector<T> merge(const std::vector<T>& lhs, const std::vector<T>& rhs)
 {
-    return std::vector<T>();
+    std::vector<T> result;
+    result.reserve(lhs.size() + rhs.size());
+
+    auto itRhs = std::begin(rhs);
+    auto itLhs = std::begin(lhs);
+    //auto itResult = std::begin(result);
+
+    if (itLhs == std::end(lhs))
+    {
+        std::copy(itRhs, std::end(rhs), std::back_inserter(result));
+    }
+    else if (itRhs == std::end(rhs))
+    {
+        std::copy(itLhs, std::end(lhs), std::back_inserter(result));
+    }
+
+    // For debugging
+    // printVector(result);
+
+    return result;
 }
 
 // Given a vector of sorted vectors, merge them into a new sorted vector.
--- src/merge_arrays/merge_vectors_test.cpp
+++ src/merge_arrays/merge_vectors_test.cpp
@@ -5,26 +5,56 @@
 #include "merge_vectors.h"
 #include <vector>
 
+static const std::vector<float> empty;
+static const std::vector<float> vector1 {1.0f, 2.0f, 3.0f};
+
+void assertVectorsEqual(const std::vector<float>& expected, const std::vector<float>& actual)
+{
+    BOOST_CHECK_EQUAL_COLLECTIONS(std::begin(expected), std::end(expected), std::begin(actual), std::end(actual));
+}
+
 BOOST_AUTO_TEST_CASE(Merge_TwoEmptyFloatVectors_ReturnsEmptyVector)
 {
     // Arrange
-    std::vector<float> lhs, rhs;
+    const auto& lhs = empty;
+    const auto& rhs = empty;
+    const auto& expected = empty;
 
     // Act
     auto result = merge(lhs, rhs);
 
     // Assert
-    BOOST_CHECK_EQUAL(0u, result.size());
+    assertVectorsEqual(expected, result);
 }
 
-// BOOST_AUTO_TEST_CASE(Merge_OneEmptyOneSortedVector_ReturnsCopyOfSortedVector)
-// {
-// }
-//
-// BOOST_AUTO_TEST_CASE(Merge_OneSortedOneEmptyVector_ReturnsCopyOfSortedVector)
-// {
-// }
-//
+BOOST_AUTO_TEST_CASE(Merge_OneEmptyOneSortedVector_ReturnsCopyOfSortedVector)
+{
+    // Arrange
+    const auto& lhs = empty;
+    const auto& rhs = vector1;
+    const auto& expected = vector1;
+
+    // Act
+    auto result = merge(lhs, rhs);
+
+    // Assert
+    assertVectorsEqual(expected, result);
+}
+
+BOOST_AUTO_TEST_CASE(Merge_OneSortedOneEmptyVector_ReturnsCopyOfSortedVector)
+{
+    // Arrange
+    const auto& lhs = vector1;
+    const auto& rhs = empty;
+    const auto& expected = vector1;
+
+    // Act
+    auto result = merge(lhs, rhs);
+
+    // Assert
+    assertVectorsEqual(expected, result);
+}
+
 // BOOST_AUTO_TEST_CASE(Merge_TwoFloatVectorsLengthThree_MergesVectors)
 // {
 // }
